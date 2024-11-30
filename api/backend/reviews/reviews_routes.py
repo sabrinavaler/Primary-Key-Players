0d51fb1559b699a4ce2f878b3b58cb0e60b615f4
alliesclark <alliesusanclark@gmail.com>
2024-11-30 09:42:31 -0800

    Get route: reviews about a specific job position

--- api/backend/reviews/reviews_routes.py
+++ api/backend/reviews/reviews_routes.py
@@ -239,124 +239,31 @@ def get_reviews_by_student(student_id):
     response.status_code = 200
     return response
     
+
+
 # ------------------------------------------------------------
-# Get the top 5 most expensive products from the database
-@students.route('/mostExpensive')
-def get_most_pop_products():
+# Retrieve a list of reviews about a specific job position
+@reviews.route('/reviews/job-position/<id>', methods=['GET'])
+def get_reviews_by_job_position(job_position_id):
 
     query = '''
-        SELECT product_code, 
-               product_name, 
-               list_price, 
-               reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 5
+        SELECT r.id, r.rating, r.review, r.student_id, r.job_position_id
+        FROM review
+        WHERE job_position_id = {str(job_position_id)}
     '''
-    
-    # Same process as handler above
+    #Log query
+    current_app.logger.info(f'GET /reviews/job-position/<id> query={query}')
+
+    # Get a cursor object from the database
     cursor = db.get_db().cursor()
+
+    # Execute the query
     cursor.execute(query)
+
+    # Fetch all matching records
     theData = cursor.fetchall()
- 
+
+    # Create an HTTP response with the data
     response = make_response(jsonify(theData))
     response.status_code = 200
     return response
-
-# ------------------------------------------------------------
-# Route to get the 10 most expensive items from the 
-# database.
-@students.route('/tenMostExpensive', methods=['GET'])
-def get_10_most_expensive_products():
-    
-    query = '''
-        SELECT product_code, 
-               product_name, 
-               list_price, 
-               reorder_level
-        FROM products
-        ORDER BY list_price DESC
-        LIMIT 10
-    '''
-    
-    # Same process as above
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    theData = cursor.fetchall()
-    
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response
-    
-
-# ------------------------------------------------------------
-# This is a POST route to add a new product.
-# Remember, we are using POST routes to create new entries
-# in the database. 
-@students.route('/product', methods=['POST'])
-def add_new_product():
-    
-    # In a POST request, there is a 
-    # collecting data from the request object 
-    the_data = request.json
-    current_app.logger.info(the_data)
-
-    #extracting the variable
-    name = the_data['product_name']
-    description = the_data['product_description']
-    price = the_data['product_price']
-    category = the_data['product_category']
-    
-    query = f'''
-        INSERT INTO products (product_name,
-                              description,
-                              category, 
-                              list_price)
-        VALUES ('{name}', '{description}', '{category}', {str(price)})
-    '''
-    # TODO: Make sure the version of the query above works properly
-    # Constructing the query
-    # query = 'insert into products (product_name, description, category, list_price) values ("'
-    # query += name + '", "'
-    # query += description + '", "'
-    # query += category + '", '
-    # query += str(price) + ')'
-    current_app.logger.info(query)
-
-    # executing and committing the insert statement 
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-    
-    response = make_response("Successfully added product")
-    response.status_code = 200
-    return response
-
-# ------------------------------------------------------------
-### Get all product categories
-@students.route('/categories', methods = ['GET'])
-def get_all_categories():
-    query = '''
-        SELECT DISTINCT category AS label, category as value
-        FROM products
-        WHERE category IS NOT NULL
-        ORDER BY category
-    '''
-
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    theData = cursor.fetchall()
-        
-    response = make_response(jsonify(theData))
-    response.status_code = 200
-    return response
-
-# ------------------------------------------------------------
-# This is a stubbed route to update a product in the catalog
-# The SQL query would be an UPDATE. 
-@students.route('/product', methods = ['PUT'])
-def update_product():
-    product_info = request.json
-    current_app.logger.info(product_info)
-
-    return "Success"
